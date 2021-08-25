9a3c9ea3434cedb0de76139dea7e36258993d90e
kyh196201 <kyh196201@gmail.com>
2021-08-25 14:41:44 +0900

    틱택토 컴퓨터 모드 추가

--- tic tac toe/index.js
+++ tic tac toe/index.js
@@ -2,11 +2,15 @@
 
 const LENGTH = 3;
 const marks = ['O', 'X'];
-const players = ['player1', 'player2'];
+let mode = 'single';
+
+const players =
+  mode === 'multi' ? ['player1', 'player2'] : ['player1', 'computer'];
 
 let currentPlayers = players.slice();
-let turn = 0;
+let count = 0;
 let board = null;
+let turn = currentPlayers[0];
 let gameover = false;
 
 // 엘리먼트
@@ -18,17 +22,26 @@ const $mark = document.getElementById('mark');
 function resetGame() {
   currentPlayers = players.slice();
   gameover = false;
-  turn = 0;
+  count = 0;
+  turn = currentPlayers[0];
 
   setBoard();
   renderBoard();
 }
 
+// 게임 종료
 function endGame(message) {
   gameover = true;
 
-  alert(message);
-  resetGame();
+  setTimeout(() => {
+    alert(message);
+    resetGame();
+  }, 50);
+}
+
+// 컴퓨터 차례인지 체크
+function isComputersTurn() {
+  return mode === 'single' && turn === 'computer';
 }
 
 // 가로 체크
@@ -47,25 +60,24 @@ function checkVerticalCompletion(y, mark) {
   return true;
 }
 
-// 클릭 이벤트
-function handleClickCell(e) {
-  if (gameover) return false;
+// x, y 좌표에 해당하는 셀 엘리먼트 찾기
+function findCell(x, y) {
+  const selector = `[data-x="${x}"][data-y="${y}"]`;
 
-  const $target = e.target;
+  return document.querySelector(selector);
+}
 
-  if ($target.className !== 'cell') return false;
-
-  const { x, y } = $target.dataset;
-
-  // 이미 값이 있을 경우
-  if (!!board[x][y]) return false;
-
-  const mark = marks[turn % 2];
-  const player = currentPlayers[turn % 2];
+// x, y 좌표에 해당하는 셀 선택
+function selectCell(x, y) {
+  const mark = marks[count % 2];
+  const player = currentPlayers[count % 2];
 
   // 칸 추가
   board[x][y] = mark;
-  $target.textContent = mark;
+  const $cell = findCell(x, y);
+
+  if (!$cell) return false;
+  $cell.textContent = mark;
 
   // 가로 체크
   if (checkHorizontalCompletion(x, mark)) {
@@ -93,15 +105,84 @@ function handleClickCell(e) {
   }
 
   // 무승부
-  if (turn === 8) {
+  if (count === 8) {
     endGame('무승부입니다.');
     return false;
   }
 
   // 다음 턴으로
-  turn += 1;
+  count += 1;
+  turn = currentPlayers[count % 2];
   renderMark();
   renderPlayer();
+}
+
+// 클릭 이벤트
+function handleClickCell(e) {
+  if (gameover) return false;
+
+  if (isComputersTurn()) {
+    console.log('컴퓨터 차례입니다. 잠시만 기다려주세요.');
+    return false;
+  }
+
+  const $target = e.target;
+
+  if ($target.className !== 'cell') return false;
+
+  const { x, y } = $target.dataset;
+
+  // 이미 값이 있을 경우
+  if (!!board[x][y]) return false;
+
+  selectCell(x, y);
+
+  if (isComputersTurn()) {
+    console.log('컴퓨터 차례입니다.');
+    playComputer();
+  }
+}
+
+function findEmptyCells() {
+  const coords = [];
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (!board[i][j]) {
+        const coord = [i, j];
+        coords.push(coord);
+      }
+    }
+  }
+
+  return coords;
+}
+
+function getRandomIndex(max = 1) {
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomCoords() {
+  const emptyCells = findEmptyCells();
+
+  if (!emptyCells.length) {
+    console.error('에러');
+  }
+
+  const randomIndex = getRandomIndex(emptyCells.length);
+
+  const [x, y] = emptyCells[randomIndex];
+
+  return [x, y];
+}
+
+// 컴퓨터 차례 실행
+function playComputer() {
+  const [x, y] = getRandomCoords();
+
+  setTimeout(() => {
+    selectCell(x, y);
+  }, 500);
 }
 
 // 셀 엘리먼트 생성
@@ -139,20 +220,24 @@ function renderBoard() {
 
 // 현재 플레이어 화면에 렌더링
 function renderPlayer() {
-  $player.textContent = currentPlayers[turn % 2];
+  $player.textContent = currentPlayers[count % 2];
 }
 
 // 마크 화면에 렌더링
 function renderMark() {
-  $mark.textContent = marks[turn % 2];
+  $mark.textContent = marks[count % 2];
 }
 
-// init
-function init() {
+function initGame() {
   setBoard();
   renderBoard();
   renderPlayer();
   renderMark();
+}
+
+// init
+function init() {
+  initGame();
 }
 
 init();
